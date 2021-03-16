5e0a199781ae309263dfec1570bae44fd69793bc
karolyim <matej.karolyi@gmail.com>
2021-03-16 15:27:14 +0100

    [FEATURE] task three (larger font button added)

--- main.js
+++ main.js
@@ -8,3 +8,15 @@
 function prepniZcervenani(elementId) {
     document.getElementById(elementId).classList.toggle('cerveny-text');
 }
+
+/**
+ * Funkce zvětší/změnší písmo v elementu o definovanou velikost
+ * 
+ * @param {string} elementSelector 
+ * @param {int} fontSizeChange 
+ */
+function zvetsitPismo(elementSelector, fontSizeChange) {
+    let element = document.querySelector(elementSelector);
+    let currentFontSize = parseInt(element.style.fontSize);
+    element.style.fontSize = (currentFontSize + fontSizeChange) + 'px';
+}
