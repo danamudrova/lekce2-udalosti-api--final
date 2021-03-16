e830ce930e7455135bc1cadad6d142d99e3e791a
karolyim <matej.karolyi@gmail.com>
2021-03-16 15:25:41 +0100

    [FEATURE] task two (red font button added)

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+
+/**
+ * Funkce přidá nebo odebere na prvku třídu, která se stará o červenou barvu písma
+ * 
+ * @param {string} elementId 
+ */
+function prepniZcervenani(elementId) {
+    document.getElementById(elementId).classList.toggle('cerveny-text');
+}
